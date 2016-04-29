68dcf120913febd646f259ae29f003f3abcb3f36
dom <dom@domlib.com>
2016-04-29 16:04:37 +0800

    add some scripts

---
+++ scripts/loader.ts
@@ -0,0 +1,115 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  TSPack is released under the MIT license:
+//  
+//  Copyright (c) TSPack.org
+//  All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of 
+//  this software and associated documentation files (the "Software"), to deal in the
+//  Software without restriction, including without limitation the rights to use, copy,
+//  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+//  and to permit persons to whom the Software is furnished to do so, subject to the 
+//  following conditions:
+//  
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT 
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION 
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//  
+//////////////////////////////////////////////////////////////////////////////////////
+
+namespace ts {
+    /**
+     * Start loading a json file.
+     */
+    function loadJSON(url:string, callback:(data:any)=>void):void {
+        let xhr = window["XMLHttpRequest"] ? new XMLHttpRequest() :
+            new ActiveXObject('Microsoft.XMLHTTP');
+        xhr.open('get', url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                let status = xhr.status;
+                if (status == 200) {
+                    let data = JSON.parse(xhr.responseText);
+                    callback(data);
+                }
+            }
+        };
+        xhr.send();
+    }
+    /**
+     * Start loading all scripts in the list.
+     */
+    function loadAll(list:string[], callback:()=>void) {
+        let loadNext = ()=> {
+            let url = list.shift();
+            loadScript(url, () => {
+                if (list.length > 0) {
+                    loadNext();
+                }
+                else {
+                    callback && callback();
+                }
+            })
+        };
+        loadNext();
+    }
+    /**
+     * Start loading one script.
+     */
+    function loadScript(src:string, callback:()=>void):void {
+        let node = document.createElement('script');
+        node.type = "text/javascript";
+        node.async = true;
+        node.charset = 'utf-8';
+        node.addEventListener('load', function () {
+            node.parentNode.removeChild(node);
+            this.removeEventListener('load', arguments.callee, false);
+            callback();
+        }, false);
+        node.src = src;
+        document.body.appendChild(node);
+    }
+    /**
+     * @language en_US
+     * Start loading the manifest.json file and all script files in it.
+     * @param url The path of the manifest.json file.
+     * @param callback The function to be called when load is complete.
+     */
+    /**
+     * @language zh_CN
+     * 开始加载manifest.json文件以及它内部记录的脚本文件列表。
+     * @param url manifest.json的文件路径。
+     * @param callback 当加载完成时的回调函数。
+     */
+    export function loadManifest(url:string, callback?:()=>void):void {
+        loadJSON(url, (data:any)=> {
+            url = url.split("\\").join("/");
+            let index = url.lastIndexOf("/");
+            let files:string[] = data.files;
+            if (index != -1) {
+                let baseURL = url.substring(0, index + 1);
+                for (let i = 0; i < files.length; i++) {
+                    files[i] = baseURL + files[i];
+                }
+            }
+            loadAll(files, callback);
+        })
+    }
+}
+
+//check the data-manifest property in script node, and load it automatically if find one.
+let scripts = document.getElementsByTagName('script');
+for (let i = 0, l = scripts.length; i < l; i++) {
+    let node = scripts[i];
+    let url = node.getAttribute('data-manifest');
+    if (url) {
+        ts.loadManifest(url);
+    }
+}
---
+++ scripts/reflection.d.ts
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////////////////////
+//
+//  TSPack is released under the MIT license:
+//
+//  Copyright (c) TSPack.org
+//  All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of
+//  this software and associated documentation files (the "Software"), to deal in the
+//  Software without restriction, including without limitation the rights to use, copy,
+//  modify, merge, publish, distribute, sublicense, and/or sell copies of the Software,
+//  and to permit persons to whom the Software is furnished to do so, subject to the
+//  following conditions:
+//
+//      The above copyright notice and this permission notice shall be included in all
+//      copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+//  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+//  PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+//  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+//  SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//////////////////////////////////////////////////////////////////////////////////////
+
+declare namespace ts {
+    /**
+     * @language en_US
+     * Returns a reference to the class object of the class specified by the name parameter.
+     * @param name The name of a class.
+     */
+    /**
+     * @language zh_CN
+     * 返回 name 参数指定的类的类对象引用。
+     * @param name 类的名称。
+     */
+    function getDefinitionByName(name: string): any;
+    /**
+     * @language en_US
+     * Return the fully qualified class name of an object
+     * @param value The object for which a fully qualified class name is desired. Any JavaScript value may be passed to
+     * this method including all available JavaScript types, object instances, primitive types such as number, and class objects.
+     * @returns A string containing the fully qualified class name.
+     */
+    /**
+     * @language zh_CN
+     * 返回对象的完全限定类名。
+     * @param value 需要完全限定类名称的对象，可以将任何 JavaScript 值传递给此方法，包括所有可用的 JavaScript 类型、对象实例、原始类型
+     * （如number)和类对象
+     * @returns 包含完全限定类名称的字符串。
+     */
+    function getQualifiedClassName(value: any): string;
+    /** @language en_US
+     * Returns the fully qualified class name of the base class of the object specified by the value parameter.
+     * @param value The object for which a parent class is desired. Any JavaScript value may be passed to this method including
+     * all available JavaScript types, object instances, primitive types such as number, and class objects.
+     * @returns  A fully qualified base class name, or null if none exists.
+     */
+    /**
+     * @language zh_CN
+     * 返回 value 参数指定的对象的基类的完全限定类名。
+     * @param value 需要取得父类的对象，可以将任何 JavaScript 值传递给此方法，包括所有可用的 JavaScript 类型、对象实例、原始类型（如number）和类对象
+     * @returns 完全限定的基类名称，或 null（如果不存在基类名称）。
+     */
+    function getQualifiedSuperclassName(value: any): string;
+    /**
+     * @language en_US
+     * Indicates whether an object is a instance of the class or interface specified as the parameter.This method can indicate
+     * whether an object is a instance of the specific interface even though the interface does not exist at JavaScript runtime.
+     * @param instance the instance to be checked.
+     * @param typeName the string value representing a specific class or interface.
+     * @returns A value of true if the object is a instance of the class or interface specified as the parameter.
+     * @example
+     * <pre>
+     *     var instance = new ts.Sprite();
+     *     console.log(ts.is(instance,"ts.Sprite"))  //true
+     *     console.log(ts.is(instance,"ts.DisplayObjectContainer"))  //true,because ts.DisplayObjectContainer is the superclass of ts.Sprite.
+     *     console.log(ts.is(instance,"ts.Bitmap"))  //false,because ts.Bitmap is not the superclass of ts.Sprite.
+     * </pre>
+     * @see ts.registerClass()
+     */
+    /**
+     * @language zh_CN
+     * 检查指定对象是否为指定接口或类或其子类的实例。尽管JavaScript在运行时并不存在接口，此方法仍然可以判断对象是否为指定接口名对应的实例。
+     * @param instance 要判断的实例。
+     * @param typeName 类或接口的完全名称.
+     * @returns 返回true表示当前对象是指定类或接口的实例。
+     * @example
+     * <pre>
+     *     var instance = new ts.Sprite();
+     *     console.log(ts.is(instance,"ts.Sprite"))  //true
+     *     console.log(ts.is(instance,"ts.DisplayObjectContainer"))  //true，因为ts.DisplayObjectContainer是ts.Sprite的父类。
+     *     console.log(ts.is(instance,"ts.Bitmap"))  //false，因为ts.Bitmap不是ts.Sprite的父类。
+     * </pre>
+     * @see ts.registerClass()
+     */
+    function is(instance: any, typeName: string): boolean;
+}
