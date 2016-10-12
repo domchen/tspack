5b415e89a4e61acbf57b150a53ee277bb1d5d735
Dom <dom@idom.me>
2016-10-12 10:57:59 +0800

    Update the __reflect function.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -24,7 +24,7 @@
 //
 //////////////////////////////////////////////////////////////////////////////////////
 
-var __reflect = this && this.__reflect || function (p, c, t) {
+var __reflect = (this && this.__reflect) || function (p, c, t) {
     p.__class__ = c, t ? t.push(c) : t = [c], p.__types__ = p.__types__ ? t.concat(p.__types__) : t;
 }
 
